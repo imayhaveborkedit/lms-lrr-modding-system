413da12722bf135e8a0116f901490a9763570132
Daniel <dalmodovar.1337@gmail.com>
2012-11-23 17:53:14 -0500

    Added colors.py for terminal colors

--- LMS.py
+++ LMS.py
@@ -1,12 +1,52 @@
-
-
+import os
+import sys
+import subprocess
+import colors
 
 NAME = "LMS"
 VERSION = "0.0.0"
+SUBVERSION = " PRE ALPHA"
+DEBUG = True
+
+def preloadchecks():
+
+    pd(os.getcwd())
+    os.chdir(r"C:/Users/Daniel/Desktop/lrr")
+    pd(os.getcwd())
+
+    try:
+        with open('LegoRR.exe') as f:
+            pd("Executable located.")
+            pass
+    except BaseException as e:
+        p("Game not found.  Aborting.")
+
+
+def launchLRR():
+    p("Launching game...")
+    subprocess.call([r"LegoRR.exe"])
+    p("Game terminated.")
+
+def preloadobserve():
+    pass
+
+def pd(i):
+    if DEBUG: print " @ " + str(i)
+
+def p(i):
+    print " - " + str(i)
 
 def main():
-    print "Powering up LMS..."
+    p("Powering up LMS...")
+
+    pd("Gathering environment varibles...")
+    preloadobserve()
+
+    pd("Running diagnostics...")
+    preloadchecks()
+
+    colors.pc("System online.", colors.FOREGROUND_GREEN)
 
 if __name__ == '__main__':
-    print NAME + " Version " + VERSION
+    print NAME + " Version " + VERSION + SUBVERSION + "\n"
     main()
